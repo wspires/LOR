2abf516175bd0516bc5d40d1e673e6c639a8737a
Wade Spires <wade@Wade-MacBook.local>
2016-04-04 22:40:34 -0400

    Add initial project files.

---
+++ LOR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOR
+//
+//  Created by Wade Spires on 4/4/16.
+//  Copyright © 2016 Minds Aspire LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LOR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOR
+//
+//  Created by Wade Spires on 4/4/16.
+//  Copyright © 2016 Minds Aspire LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
